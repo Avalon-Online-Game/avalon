6af6e017609f4fefe585fe625bc84296ecfe7ae3
Sepehr Hashemi <sepehr.hashemih@gmail.com>
2020-06-03 00:52:32 +0430

    Fix app icon, fix api and ws base url.

--- avalon-frontend/avalon/src/utils/API.js
+++ avalon-frontend/avalon/src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+const baseUrl = __DEV__ ? 'http://localhost:8000/api/v1/' : 'https://avalongame.ir/api/v1/';
 export default axios.create({
   baseURL: 'https://avalongame.ir/api/v1/',
   responseType: 'json',
